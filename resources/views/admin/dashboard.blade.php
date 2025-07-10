292b2d502d4548c81d2409d19ec411d3ade51002
Shafwan <arul.safwan73@gmail.com>
2025-07-10 13:13:26 +0800

    design: change card information detail

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -189,11 +189,12 @@
             </div>
 
             <div class="page-body">
+                {{-- card information --}}
                 <div class="container-xl">
                     <div class="row row-deck row-cards">
                         <div class="col-12">
                             <div class="row row-cards">
-                                <div class="col-sm-6 col-lg-3">
+                                <div class="col-sm-6 col-lg-4">
                                     <div class="alert alert-primary p-4">
                                         <div class="row align-items-center">
                                             <div class="col-auto">
@@ -210,7 +211,7 @@
                                             </div>
                                             <div class="col">
                                                 <div class="font-weight-medium fw-bold">
-                                                    Siswa Perempuan
+                                                    Jumlah Siswa
                                                 </div>
                                                 <div class="text-secondary">
                                                    111
@@ -219,7 +220,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-sm-6 col-lg-3">
+                                <div class="col-sm-6 col-lg-4">
                                     <div class="alert alert-success p-4">
                                         <div class="row align-items-center">
                                             <div class="col-auto">
@@ -236,7 +237,7 @@
                                             </div>
                                             <div class="col">
                                                 <div class="font-weight-medium fw-bold">
-                                                    Siswa Laki-laki
+                                                    Jumlah Guru
                                                 </div>
                                                 <div class="text-secondary">
                                                     111
@@ -245,35 +246,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-sm-6 col-lg-3">
-                                    <div class="alert alert-warning p-4">
-                                        <div class="row align-items-center">
-                                            <div class="col-auto">
-                                                <span class="bg-warning text-white avatar">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                                        class="icon icon-tabler icons-tabler-outline icon-tabler-users">
-                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                        <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
-                                                        <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                                                        <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                                                        <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
-                                                    </svg>
-                                                </span>
-                                            </div>
-                                            <div class="col">
-                                                <div class="font-weight-medium fw-bold">
-                                                    Total Siswa
-                                                </div>
-                                                <div class="text-secondary">
-                                                    111
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6 col-lg-3">
+                                <div class="col-sm-6 col-lg-4">
                                     <div class="alert alert-danger p-4">
                                         <div class="row align-items-center">
                                             <div class="col-auto">
@@ -293,7 +266,7 @@
                                             </div>
                                             <div class="col">
                                                 <div class="font-weight-medium fw-bold">
-                                                    Siswa Tidak Aktif
+                                                    Total Pelanggar Bulan Ini
                                                 </div>
                                                 <div class="text-secondary">
                                                     111
